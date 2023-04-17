694b678c075e7b48128b8c52562e20a5cb1b87cd
adarshbende12 <104903732+adarshbende12@users.noreply.github.com>
2023-04-17 19:38:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import telebot
 bot = telebot.TeleBot('6138384898:AAEzKju6XSPYeyfojKJf5uuI9QdUwqo_9mE')
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id,f'hii {message.from_user.first_name}')
+    with open('images/bingo.jpeg','rb') as photo:
+        bot.send_photo(message,photo=photo,caption="hii ra")
 print("bot started")
 bot.polling()
